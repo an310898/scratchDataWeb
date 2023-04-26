1324e2d756458f4a29a2c4f0319b4e8771808558
anns318 <an310898@gmail.com>
2023-04-27 03:16:08 +0700

    ignore data scratch update scratch

--- index.js
+++ index.js
@@ -1,76 +1,100 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs')
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({height:1080,width:1920})
-  
-  await page.goto('https://www.fahasa.com/sach-trong-nuoc/van-hoc-trong-nuoc.html');
+  await page.setViewport({ height: 1080, width: 1920 });
 
+  // const UrlCate = [
+  //   'https://www.fahasa.com/sach-trong-nuoc/van-hoc-trong-nuoc.html?order=num_orders&limit=48&p=1',
+  //   'https://www.fahasa.com/sach-trong-nuoc/nuoi-day-con.html?order=num_orders&limit=48&p=1',
+  //   'https://www.fahasa.com/sach-trong-nuoc/thieu-nhi.html?order=num_orders&limit=48&p=1',
+  //   'https://www.fahasa.com/sach-trong-nuoc/tam-ly-ky-nang-song.html?order=num_orders&limit=48&p=1',
+  //    'https://www.fahasa.com/sach-trong-nuoc/tieu-su-hoi-ky.html?order=num_orders&limit=48&p=1'
+  // ]
   
-  await page.waitForSelector('.item-inner');
+  await page.goto(
+    "https://www.fahasa.com/sach-trong-nuoc/tieu-su-hoi-ky.html?order=num_orders&limit=48&p=1"
+  );
 
-  
+  await page.waitForSelector(".item-inner");
+
   const productDetailUrls = await page.evaluate(() => {
-    const bookElements = document.querySelectorAll('.item-inner');
+    const bookElements = document.querySelectorAll(".item-inner");
 
     const urls = [];
 
     for (let element of bookElements) {
-      const url = element.querySelector('.product > a').href;
+      const url = element.querySelector(".product > a").href;
       urls.push(url);
     }
 
     return urls;
   });
 
-  console.log(productDetailUrls)
+  console.log(productDetailUrls);
 
   const books = [];
 
   for (let url of productDetailUrls) {
+    
     await page.goto(url);
-    await page.waitForSelector('h1');
+    await page.waitForSelector("h1");
+    await page.waitForSelector("#desc_content");
 
     const book = await page.evaluate(() => {
-      const title = document.querySelector('h1').textContent.trim();
-      const author = document.querySelector('.product-view-sa-author span:last-child').textContent.trim();
-      const price = document.querySelector('.price').textContent.trim();
-      const imgSrc = document.querySelector('#image').src;
-      const description = document.querySelector('#desc_content').innerHTML;
-      const language = document.querySelector('.data_languages').textContent.trim();
-      const age = document.querySelector('.data_age > a').textContent.trim();
-      const publisher = document.querySelector('.data_publisher').textContent.trim();
-
-      // console.log(description)
-      // const details = {};
-
-      // const detailRows = document.querySelectorAll('.book-attribute tr');
-
-      // for (let row of detailRows) {
-      //   const label = row.querySelector('td:first-child').textContent.trim();
-      //   const value = row.querySelector('td:last-child').textContent.trim();
-      //   details[label] = value;
-      // }
-
+      const Name = document.querySelector("h1").textContent.trim();
+      const author = document .querySelector(".product-view-sa-author span:last-child")?.textContent.trim() || '';
+      const price = document.querySelector(".price").textContent.trim();
+      const image = document.querySelector("#image")?.src;
+      const description = document.querySelector("#desc_content")?.innerHTML || 'Chưa có mô tả';
+      const language = document .querySelector(".data_languages")?.textContent.trim() || '';
+      const age = document.querySelector(".data_age > a")?.textContent.trim() || 0;
+      const publisher = document .querySelector(".data_publisher")?.textContent.trim() || '';
+      const pages = document .querySelector(".data_qty_of_page")?.textContent.trim() || 0;
+      const cover = document .querySelector(".data_book_layout")?.textContent.trim() || '';
+      const weight = document .querySelector(".data_weight")?.textContent.trim() || '';
+      const size = document.querySelector(".data_size")?.textContent.trim() || '';
+      
       return {
-        title,
+        Name,
         author,
         price,
-        imgSrc,
+        image,
         description,
         language,
         age,
-        publisher
-        
+        publisher,
+        pages,
+        cover,
+        weight,
+        size,
       };
     });
-
+    console.log(book)
     books.push(book);
   }
 
-  console.log(books[0].title);
-  fs.appendFileSync('data.txt', "INSERT INTO dbo.Book(Name, Description, Image, Author, Price, Age, Language, Pages, PublishingCompany, IsHidden, CreatedDate ) VALUES (   N'',     -- Name - nvarchar(250) NULL,    -- Description - nvarchar(max) NULL,    -- Image - nvarchar(max) N'',     -- Author - nvarchar(250) '',      -- Price - varchar(25) NULL,    -- Age - int NULL,    -- Language - nvarchar(250) NULL,    -- Pages - int NULL,    -- PublishingCompany - nvarchar(250) DEFAULT, -- IsHidden - bit DEFAULT  -- CreatedDate - date )")
+  for (let book of books) {
+    console.log(book);
+    fs.appendFileSync('tieu-su-hoi-ky.txt', 
+    `EXEC dbo.AddNewBook @Name = N'${book.Name}',              -- nvarchar(250)
+    @Description = N'${book.description}',       -- nvarchar(max)
+    @Image = N'${book.image}',             -- nvarchar(max)
+    @Author = N'${book.author}',            -- nvarchar(250)
+    @Price = '${book.price.replace('đ','').trim()}',              -- varchar(25)
+    @Age = '${book.age}',                 -- int
+    @Language = N'${book.language}',          -- nvarchar(250)
+    @Pages = '${book.pages}',               -- int
+    @PublishingCompany = N'${book.publisher}', -- nvarchar(250)
+    @CoverType = N'${book.cover}',         -- nvarchar(250)
+    @BookWeight = N'${book.weight}',        -- nvarchar(25)
+    @BookSize = N'${book.size}',          -- nvarchar(50)
+    @CategoryIdArrays = '5'    -- varchar(max)
+`)
+  }
 
   await browser.close();
 })();
